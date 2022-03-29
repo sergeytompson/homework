afea90a9698fe514a8be6519e93121db6ab025d9
sergeytompson <gfhfahfp@gmail.com>
2022-03-29 20:29:19 +0300

    lesson_4 исправление 2

--- 03_shape_select.py
+++ 03_shape_select.py
@@ -21,77 +21,76 @@ def figure(vector, injection, deviation, repeat, long, figure_color):
     sd.line(point, end, width=3, color=figure_color)
 
 
-def triangle(point, injection, long, triangle_color):
+def triangle(point, injection, long, color):
     vector = sd.get_vector(point, injection, long, width=3)
-    figure(vector, injection, 120, 2, long, figure_color=triangle_color)
+    figure(vector, injection, 120, 2, long, figure_color=color)
 
 
-def square(point, injection, long, square_color):
+def square(point, injection, long, color):
     vector = sd.get_vector(point, injection, long, width=3)
-    figure(vector, injection, 90, 3, long, figure_color=square_color)
+    figure(vector, injection, 90, 3, long, figure_color=color)
 
 
-def pentagon(point, injection, long, pentagon_color):
+def pentagon(point, injection, long, color):
     vector = sd.get_vector(point, injection, long, width=3)
-    figure(vector, injection, 72.1, 4, long, figure_color=pentagon_color)
+    figure(vector, injection, long, 72.1, 4, figure_color=color)
 
 
-def hexagon(point, injection, long, hexagon_color):
+def hexagon(point, injection, long, color):
     vector = sd.get_vector(point, injection, long, width=3)
-    figure(vector, injection, 60, 5, long, figure_color=hexagon_color)
+    figure(vector, injection, 60, 5, long, figure_color=color)
 
 
 colors = [('red', sd.COLOR_RED), ('orange', sd.COLOR_ORANGE), ('yellow', sd.COLOR_YELLOW),
           ('green', sd.COLOR_GREEN), ('cyan', sd.COLOR_CYAN), ('blue', sd.COLOR_BLUE),
           ('purple', sd.COLOR_PURPLE)]
 
+figures = [('triangle', triangle), ('square', square), ('pentagon', pentagon), ('hexagon', hexagon)]
 
+print('Возможные фигуры:')
+for num, figure in enumerate(figures):
+    print(f'{num}: {figure[0]}')
 
 print('Возможные цвета:')
-for num, color in enumerate(colors):
-    print(f'{num}: {color[0]}')
+for num, color_from_colors in enumerate(colors):
+    print(f'{num}: {color_from_colors[0]}')
 
 
-def check_input():
+def check_input_figure():
     while True:
-        # TODO где то здесь должен быть выбор фигур
-        """
-        в списке ты можешь так же хранить функции, например
-        figures = [
-            ('triangle', triangle), ('hexagon', hexagon)
-]
-        ты можешь получить аналогично цвету нужную тебе функцию и записать ее в переменную и вызвать:
-        
-        def get_func_from_input():
-            ...
-            return func
-        
-        shape_func = get_func_from_input()
-        shape_func()
-        
-        """
-        num = input('Введите число от 0 до 6, соответствующее нужному цвету: ')
-        if not num.isdigit():
+        figure_num = input('Введите число от 0 до 3, соответствующее нужной фигуре: ')
+        if not figure_num.isdigit():
             print('Ошибка')
             continue
-        num = int(num)
-        if not 0 <= num <= 6:
+        figure_num = int(figure_num)
+        if not 0 <= figure_num <= 3:
             print('Ошибка')
             continue
         else:
             break
-    return num
+    return figure_num
+
+
+def check_input_color():
+    while True:
+        color_num = input('Введите число от 0 до 6, соответствующее нужному цвету: ')
+        if not color_num.isdigit():
+            print('Ошибка')
+            continue
+        color_num = int(color_num)
+        if not 0 <= color_num <= 6:
+            print('Ошибка')
+            continue
+        else:
+            break
+    return color_num
 
 
 if __name__ == '__main__':
-    num = check_input()
-    color = colors[num][1]
-    point_4 = sd.get_point(100, 500)
-    hexagon(point_4, 120, 50, hexagon_color=color)
-    point_3 = sd.get_point(100, 100)
-    pentagon(point_3, 60, 50, pentagon_color=color)
-    point_2 = sd.get_point(500, 100)
-    square(point_2, 20, 50, square_color=color)
-    point_1 = sd.get_point(500, 500)
-    triangle(point_1, 40, 50, triangle_color=color)
+    figure_number = check_input_figure()
+    chosen_figure = figures[figure_number][1]
+    color_number = check_input_color()
+    chosen_color = colors[color_number][1]
+    figure_point = sd.get_point(300, 300)
+    chosen_figure(point=figure_point, injection=30, long=100, color=chosen_color)
     sd.pause()
